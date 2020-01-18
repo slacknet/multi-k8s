4a92929331c9c5e38536efe5dc911a5d3f57dfe3
cyberk <stefaniu.services@gmail.com>
2020-01-18 22:38:07 +0000

    changed the logo again

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <img src="fibyellow.png" className="App-logo" alt="logo" />
+            <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Fibonacci Calculator</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
