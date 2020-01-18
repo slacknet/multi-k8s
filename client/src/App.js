7813e50c83388a3dd9f4d03338d676f3b4ad97ea
cyberk <stefaniu.services@gmail.com>
2020-01-18 10:09:23 +0000

    changes in app.js and adding logo

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,8 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <img src='fibyellow.png' className="App-logo" alt="logo" />
+            <h1 className="App-title">Fibonacci Calculator</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
